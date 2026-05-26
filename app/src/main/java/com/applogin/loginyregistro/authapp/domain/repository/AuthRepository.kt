ddbea9718cd06a9caf2b507bff0d5baff374a2b6
pachospm <pachosp123@gmail.com>
2026-05-25 19:39:53 -0500

    feat: se creo carpeta domain con sus respectivas subcarpetas y clases

--- app/src/main/java/com/applogin/loginyregistro/authapp/domain/repository/AuthRepository.kt
+++ app/src/main/java/com/applogin/loginyregistro/authapp/domain/repository/AuthRepository.kt
@@ -1,4 +1,19 @@
 package com.applogin.loginyregistro.authapp.domain.repository
 
+import okhttp3.Response
+import com.applogin.loginyregistro.authapp.domain.model.User
+import kotlinx.coroutines.flow.Flow
+
 interface AuthRepository {
+    //Flujo que emite el token guardado o null si no existe la sesión
+    val token: Flow<String>
+    // Registrar un usuario usando nambe,email y password
+    //Devolver Resource<String> porque el backedn retorna un mensaje
+    suspend fun register(name: String, email: String, password:String): Resource<String>
+    suspend fun login(email: String, password: String): Resource<User>
+
+    //Consulta el perfil de usuairo autenticado usando el token guardado
+    suspend fun getProfile(): Resource<User>
+    // Cierra sesión
+    suspend fun logout()
 }
